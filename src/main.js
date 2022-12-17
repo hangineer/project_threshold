14d3d33c92bf91a862fd4f6fccf8e0618014423b
hannah <hanforwork896@gmail.com>
2022-12-18 03:16:40 +0800

    feat:project setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap"
+// import '@/css/bootstrap-custom.scss'; 
+//css-reset
+import "reset-css";
+
 createApp(App).mount('#app')
