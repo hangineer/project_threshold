381a01b265b82081b892f97e2970a4d196fce489
hannah <hanforwork896@gmail.com>
2022-12-30 02:29:13 +0800

    feat: render places on page from db.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
+import { createApp } from 'vue';
+import App from './App.vue';
+// import axios from 'axios';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap"
@@ -8,6 +8,13 @@ import "bootstrap"
 
 //css-reset
 import "reset-css";
+
+//axios instance
+// const instance = axios.create({
+//     baseURL: "http://localhost:3000/",
+//     headers: { 'Content-Type': 'application/json' },
+//     // headers: { "X-Requested-With": "XMLHttpRequest" },
+//   });
 
 //element-plus
 // import ElementPlus from 'element-plus'
